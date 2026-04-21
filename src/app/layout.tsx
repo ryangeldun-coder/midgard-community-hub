0ac20b463a93af7313cd88f48f8f03d44bebb315
Midgard Community Hub <you@example.com>
2026-04-21 16:46:55 +0200

    chore: add adsense verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7864174863449248" />
         <Script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7864174863449248"
